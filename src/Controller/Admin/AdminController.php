b7af989e785ef9268ba96da792b6bc38b33b5f91
Parasmo Marco <115583768+encoreuntictac@users.noreply.github.com>
2023-08-15 04:12:48 +0200

    Ajout du formulaire d'ajout equipment.

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SpaceCategories;
+use App\Entity\SpaceEquipements;
 use App\Form\SpaceCategFormType;
+use App\Form\SpaceEquipmentFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +27,8 @@ class AdminController extends AbstractController
         Request $request,
         EntityManagerInterface $entityManager
     ): Response {
+        $type = 'storages';
+
         $spaceCategories = new SpaceCategories();
         $formNew = $this->createForm(SpaceCategFormType::class, $spaceCategories);
         $formNew->handleRequest($request);
@@ -38,7 +42,7 @@ class AdminController extends AbstractController
             return $this->redirectToRoute('app_admin_storages', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('admin/admin/_storages.html.twig', compact('formNew', 'list'));
+        return $this->render('admin/admin/_storages.html.twig', compact('formNew', 'list', 'type'));
     }
 
     #[Route('/admin/storages/{id}/edit', name: 'app_admin_storages_edit', methods: ['GET', 'POST'])]
@@ -63,7 +67,7 @@ class AdminController extends AbstractController
         return $this->render('admin/admin/_storages.html.twig', compact('formEdit', 'list', 'itemName'));
     }
 
-    #[Route('/admin/storages/{id}', name: 'app_admin_storage_delete', methods: ['POST'])]
+    #[Route('/admin/storages/{id}', name: 'app_admin_storages_delete', methods: ['POST'])]
     public function storageDelete(
         Request $request, 
         EntityManagerInterface $entityManager,
@@ -78,10 +82,61 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/equipments', name: 'app_admin_equipments')]
-    public function equipments(): Response
-    {
-        return $this->render('admin/admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+    public function equipments(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $type = 'equipments';
+
+        $spaceEquipement = new SpaceEquipements();
+        $formNew = $this->createForm(SpaceEquipmentFormType::class, $spaceEquipement);
+        $formNew->handleRequest($request);
+
+        $list = $entityManager->getRepository(SpaceEquipements::class)->findBy([], ['name' => 'ASC']);
+
+        if ($formNew->isSubmitted() && $formNew->isValid()) {
+            $entityManager->persist($spaceEquipement);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_equipments', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/admin/_equipments.html.twig', compact('formNew', 'list', 'type'));
+    }
+
+    #[Route('/admin/equipments/{id}/edit', name: 'app_admin_equipments_edit', methods: ['GET', 'POST'])]
+    public function equipmentsEdit(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SpaceEquipements $spaceEquipements
+    ): Response {
+        $formEdit = $this->createForm(SpaceEquipmentFormType::class, $spaceEquipements);
+        $formEdit->handleRequest($request);
+        
+        $itemName = $spaceEquipements->getName();
+        $list = $entityManager->getRepository(SpaceEquipements::class)->findBy([], ['name' => 'ASC']);
+
+        if ($formEdit->isSubmitted() && $formEdit->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_equipments', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $this->addFlash('edit', "");
+        return $this->render('admin/admin/_equipments.html.twig', compact('formEdit', 'list', 'itemName'));
+    }
+
+    #[Route('/admin/equipments/{id}', name: 'app_admin_equipments_delete', methods: ['POST'])]
+    public function equipmentDelete(
+        Request $request, 
+        EntityManagerInterface $entityManager,
+        SpaceEquipements $spaceEquipements
+    ): Response {
+        if ($this->isCsrfTokenValid('delete'.$spaceEquipements->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($spaceEquipements);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_equipments', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Entity/SpaceEquipements.php
+++ src/Entity/SpaceEquipements.php
@@ -26,6 +26,11 @@ class SpaceEquipements
         $this->equipment = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
