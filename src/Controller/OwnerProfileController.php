18376252d2876164c760a0cb4720335ce57121ae
Parasmo Marco <115583768+encoreuntictac@users.noreply.github.com>
2023-08-18 04:27:35 +0200

    La page de profile detail est partiellement fonctionnelle.

---
+++ src/Controller/OwnerProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OwnerProfileController extends AbstractController
+{
+    #[Route('/owner/profile/{id}', name: 'app_owner_profile')]
+    public function index(
+        Users $user
+    ): Response {
+        return $this->render('owner_profile/index.html.twig', compact('user'));
+    }
+}
